758d88bb89bc48e8fcb1ceb80b2e0d9e8ab74396
Michael Egwu <micair@Michaels-Air.lan>
2026-04-30 18:27:41 -0700

    Finalize Antinote floating/resize workaround

--- aerospace/on_workspace_change.sh
+++ aerospace/on_workspace_change.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-# Only notify Sketchybar of the change, don't force Zen Mode
+# Notify Sketchybar
 /opt/homebrew/bin/sketchybar --trigger aerospace_workspace_change FOCUSED_WORKSPACE="$AEROSPACE_FOCUSED_WORKSPACE"
+
+# Simple enforcement: if Antinote is on the current workspace, snap its size
+ANTINOTE_WID=$(/opt/homebrew/bin/aerospace list-windows --workspace "$AEROSPACE_FOCUSED_WORKSPACE" --app-bundle-id com.chabomakers.Antinote --format "%{window-id}")
+
+if [ ! -z "$ANTINOTE_WID" ]; then
+    sleep 0.2
+    osascript -e 'tell application "System Events" to tell process "Antinote" to set size of window 1 to {500, 500}'
+fi
